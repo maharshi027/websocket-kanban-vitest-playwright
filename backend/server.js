db26ad04d0f760b6722f203ee25a1a19e8424ae0
Harshit <harshkush15sep@gmail.com>
2026-02-16 11:30:14 +0530

    CORs updating

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "https://websocket-kanban-vitest-playwright-beryl.vercel.app",
+    origin: '*',
     methods: ["GET", "POST"]
   }
 });
